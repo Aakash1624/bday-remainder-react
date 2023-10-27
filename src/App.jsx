08c31dae08fe0dc8052f42e84f34a8ec69639ed2
AAKASH1624 <hariaakash.405@gmail.com>
2023-10-27 06:27:40 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,71 @@
-import { useState } from 'react';
-import './App.css';
+import React, { useState } from 'react';
 
-const info = [
-  { id: 1, images: 'srcimagesimg-1.jpg', title: 'Ibrahim', age: '19 years' },
+const initialInfo = [
+  {
+    id: 1,
+    images: './src/images/img-1.jpg',
+    title: 'Ibrahim',
+    age: '19 years',
+  },
   {
     id: 2,
-    images: 'srcimagesimg-2.jpg',
+    images: './src/images/img-2.jpg',
     title: 'Hester Hogan',
     age: '32 years',
   },
   {
     id: 3,
-    images: 'srcimagesimg-3.jpg',
+    images: './src/images/img-3.jpg',
     title: 'Larry Little',
     age: '36 years',
   },
-  { id: 4, images: 'srcimagesimg-4.jpg', title: 'Sean Walsh', age: '34 years' },
+  {
+    id: 4,
+    images: './src/images/img-4.jpg',
+    title: 'Sean Walsh',
+    age: '34 years',
+  },
   {
     id: 5,
-    images: 'srcimagesimg-5.jpg',
+    images: './src/images/img-5.jpg',
     title: 'Lola Gardner',
     age: '29 years',
   },
 ];
 
 function App() {
-  const [index, setIndex] = useState(5);
+  const [index, setIndex] = useState(initialInfo);
 
-  const change = () => {
-    setIndex(0);
+  const clearData = () => {
+    setIndex([]);
   };
 
-  info.map((item) => {
-    const { id, images, title, age } = item;
-  });
-
   return (
-    <>
-      <div className="main">
-        <div className="container">
-          <h1 className="title">5 Birthday today</h1>
+    <div className="main">
+      <div className="container">
+        <h1 className="title">{index.length} Birthday today</h1>
+        <div className="content-container">
           <div className="content-container">
-            <li key={item.id}>
-              <div class="person">
-                <img src={images} alt="Ibrahim" />
-                <div class="name">
-                  <h4 className="name">{title}</h4>
-                  <p className="name">{age}</p>
-                </div>
-              </div>
-            </li>
+            {index.map(({ id, images, title, age }) => {
+              return (
+                <li key={id}>
+                  <div className="person">
+                    <img src={images} alt={title} />
+                    <div className="name">
+                      <h4 className="name">{title}</h4>
+                      <p className="name">{age}</p>
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
+            <button onClick={clearData} className="btn-clear" id="btn-clear">
+              Clear All
+            </button>
           </div>
-          {/* <div class="person">
-              <img src={'/src/images/img-2.jpg'} alt="Ibrahim" />
-              <div class="name">
-                <h4 className="name">Hester Hogan</h4>
-                <p className="name">32 years</p>
-              </div>
-            </div>
-            <div class="person">
-              <img src={'/src/images/img-3.jpg'} alt="Ibrahim" />
-              <div class="name">
-                <h4 className="name">Larry Little</h4>
-                <p className="name">36 years</p>
-              </div>
-            </div>
-
-            <div class="person">
-              <img
-                src={'/src/images/img-4.jpg'}
-                alt="Ibrahim"
-                className="name4"
-              />
-              <div class="name">
-                <h4 className="name">Sean Walsh</h4>
-                <p className="name">34 years</p>
-              </div>
-            </div>
-
-            <div class="person">
-              <img
-                src={'/src/images/img-5.jpg'}
-                alt="Ibrahim"
-                className="name5"
-              />
-              <div class="name">
-                <h4 className="name">Lola Gardner</h4>
-                <p className="name">29 years</p>
-              </div>
-            </div> */}
         </div>
-
-        <button onClick={change} className="btn-clear" id="btn-clear">
-          Clear All
-        </button>
       </div>
-    </>
+    </div>
   );
 }
 
